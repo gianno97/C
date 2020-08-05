ac2363a8a37767367a46d437239641d85555da59
gianno97 <fabio.giannoccoli@gmail.com>
2020-08-05 22:54:59 +0200

    Update main.c

--- C_workspace/Matrix_project/main.c
+++ C_workspace/Matrix_project/main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 
 int controllo_zeri(int **matrice_uno, int ordine_uno, char *riga_colonna);
+int calcolo_determinante(int ordine_uno, int **matrice_uno, int risultato, char riga_colonna);
+int **riduzione_matrice(int **matrice_uno, int ordine_uno, int riga, int colonna);
 
 int main(void)
 {
-    int ordine_uno;
+    int ordine_uno, risultato, determinante;
     char riga_colonna;
     int **matrice_uno;
     int num = 1;
@@ -36,7 +38,19 @@ int main(void)
         }
     }
     
-    int risultato = controllo_zeri(matrice_uno, ordine_uno, &riga_colonna);
+    if(ordine_uno == 1)
+    {
+        determinante = ordine_uno;
+    }
+    else if(ordine_uno == 2)
+    {
+        determinante = matrice_uno[0][0] * matrice_uno[1][1] - matrice_uno[0][1] * matrice_uno[1][0];
+    }
+    else
+    {
+        risultato = controllo_zeri(matrice_uno, ordine_uno, &riga_colonna);
+        determinante = calcolo_determinante(ordine_uno, matrice_uno, risultato, riga_colonna);
+    }
     
     /*Stampiamo la matrice*/
     for(int i = 0; i < ordine_uno; i++)
@@ -53,7 +67,7 @@ int main(void)
     
 }
 
-/*
+
 int power(int base, unsigned int exp)
 {
     int result = 1;
@@ -61,12 +75,10 @@ int power(int base, unsigned int exp)
         result *= base;
     return result;
 }
-*/
 
 
 int controllo_zeri(int **matrice_uno, int ordine_uno, char *riga_colonna)
 {
-    
     int conta_zeri = 0;
     int max_zeri_riga = 0;
     int max_zeri_colonna = 0;
@@ -74,7 +86,6 @@ int controllo_zeri(int **matrice_uno, int ordine_uno, char *riga_colonna)
     int indice_colonna = 0;
     int indice_determinante;
     
- 
     /*Controllo la matrice in cerca della riga con il numero di zeri maggiore*/
     for(int i = 0; i < ordine_uno; i++)
     {
@@ -93,7 +104,6 @@ int controllo_zeri(int **matrice_uno, int ordine_uno, char *riga_colonna)
             conta_zeri = 0;
     }
     conta_zeri = 0;
-    
     
     /*Controllo la matrice in cerca della colonna con il numero di zeri maggiore*/
     for(int j = 0; j < ordine_uno; j++)
@@ -124,119 +134,84 @@ int controllo_zeri(int **matrice_uno, int ordine_uno, char *riga_colonna)
         indice_determinante = indice_colonna;
         *riga_colonna = 'c';
     }
-    
     return indice_determinante;
 }
 
-/*
-int calcolo_determinante(int ordine_uno, int matrice_uno[][ordine_uno], int matrice_ridotta[][ordine_uno - 1])
+
+int calcolo_determinante(int ordine_uno, int **matrice_uno, int risultato, char riga_colonna)
 {
     int determinante = 0;
     
-    
-    if(ordine_uno == 1)
-        determinante = ordine_uno;
-    else if(ordine_uno == 2)
+    if(riga_colonna == 'r')
     {
-        determinante = matrice_uno[0][0] * matrice_uno[1][1] - matrice_uno[0][1] * matrice_uno[1][0];
+        
     }
     else
-    {        
-        for(int j = 0; j < ordine_uno; j++)
+    {
+        if(ordine_uno == 1)
         {
-            determinante += matrice_uno[0][j] * power(-1, j) * calcolo_determinante(ordine_uno, matrice_uno[][ordine_uno], riduzione_matrice(matrice_uno[][ordine_uno]));
+            determinante = ordine_uno;
+        }
+        else if(ordine_uno == 2)
+        {
+            determinante = matrice_uno[0][0] * matrice_uno[1][1] - matrice_uno[0][1] * matrice_uno[1][0];
+        }
+        else
+        {
+            for(int i = 0; i < ordine_uno; i++)
+            {
+                determinante += matrice_uno[i][risultato] * power(-1, i + risultato) * calcolo_determinante(riduzione_matrice(matrice_uno, ordine_uno, i, risultato),
+                ordine_uno, risultato, riga_colonna);
+            }
         }
     }
-    
     return determinante;
 }
-*/
- 
-/* 
-int **riduzione_matrice(int matrice_uno[][ordine_uno])
+
+
+
+int **riduzione_matrice(int **matrice_uno, int ordine_uno, int riga, int colonna)
 {
-    
     int matrice_copia[ordine_uno][ordine_uno];
     int **matrice_ridotta;
     int ordine_ridotto = ordine_uno - 1;
+    int riga_ridotta = 0;
+    int colonna_ridotta = 0;
     matrice_ridotta = malloc(sizeof(int*) * ordine_ridotto);
     
     for(int k = 0; k < ordine_ridotto; k++)
     {
         matrice_ridotta[k] = malloc(sizeof(int*) * ordine_ridotto);
     }
-    */
     
-    /*Copio i valori della matrice principale nella matrice copia escludendo tutti i valori*/
-    /*che fanno parte della prima riga o della prima colonna*/
-    /*
+    /*Copio i valori della matrice principale nella matrice copia*/
     for(int i = 0; i < ordine_uno; i++)
     {
         for(int j = 0; j < ordine_uno; j++)
         {
-            if(matrice_uno[i][j] == matrice_uno[0][j] || matrice_uno[i][j] == matrice_uno[i][0])
-                matrice_copia[i][j] = -1;
-            else
-                matrice_copia[i][j] = matrice_uno[i][j];
-        }
-    }*/
-    
-    /*Inserisco tutti i valori diversi da -1 in una matrice ridotta*/
-    /*
-    for(int i = 1; i < ordine_ridotto; i++)
-    {
-        for(int j = 1; j < ordine_ridotto; j++)
-        {
-            matrice_ridotta[i - 1][j - 1] = matrice_uno[i][j];
+            matrice_copia[i][j] = matrice_uno[i][j];
         }
     }
     
+    /*Inserisco nella matrice ridotta tutti gli elementi che non fanno parte della riga o della colonna dell'elemento selezionato*/
+    for(int i = 0; i < ordine_uno; i++)
+    {
+        for(int j = 0; j < ordine_uno; j++)
+        {
+            if(matrice_copia[i][j] != matrice_copia[riga][j] || matrice_copia[i][j] != matrice_copia[i][colonna])
+            {
+                matrice_ridotta[riga_ridotta][colonna_ridotta] = matrice_copia[i][j];
+                if(colonna_ridotta == (ordine_ridotto - 1))
+                {
+                    colonna_ridotta = 0;
+                    riga_ridotta++;
+                }
+                else
+                {
+                    colonna_ridotta++;
+                }
+            }
+        }
+    }
     return matrice_ridotta;
 }
-*/
-
-
-/*#include <stdio.h> 
-#include <stdlib.h> 
-
-int main() 
-{ 
-
-	// This pointer will hold the 
-	// base address of the block created 
-	int* ptr; 
-	int n, i; 
-
-	// Get the number of elements for the array 
-	n = 5; 
-	printf("Enter number of elements: %d\n", n); 
-
-	// Dynamically allocate memory using malloc() 
-	ptr = (int*)malloc(n * sizeof(int)); 
-
-	// Check if the memory has been successfully 
-	// allocated by malloc or not 
-	if (ptr == NULL) { 
-		printf("Memory not allocated.\n"); 
-		exit(0); 
-	} 
-	else { 
-
-		// Memory has been successfully allocated 
-		printf("Memory successfully allocated using malloc.\n"); 
-
-		// Get the elements of the array 
-		for (i = 0; i < n; ++i) { 
-			ptr[i] = i + 1; 
-		} 
-
-		// Print the elements of the array 
-		printf("The elements of the array are: "); 
-		for (i = 0; i < n; ++i) { 
-			printf("%d, ", ptr[i]); 
-		} 
-	} 
-
-	return 0; 
-}
- */
