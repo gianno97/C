2bb1bdd8a598fcf48f424cd73af7890a952394ea
gianno97 <fabio.giannoccoli@gmail.com>
2021-01-25 11:36:49 +0100

    Update main.c

--- C_workspace/Implementazione_albero_rn/main.c
+++ C_workspace/Implementazione_albero_rn/main.c
@@ -139,3 +139,89 @@ nodo_albero_bin_rn_t *cerca_in_albero_bin_ric_rn(nodo_albero_bin_rn_t *sent_p, i
                     nodo_p->dx_p);
     return(nodo_p);
 }
+
+int inserisci_in_albero_bin_ric_rn(nodo_albero_bin_rn_t *sent_p, int valore)
+{
+    int                  inserito;
+    nodo_albero_bin_rn_t *nodo_p,
+                         *padre_p,
+                         *nuovo_p;
+    
+    for(nodo_p = sent_p->sx_p, padre_p = sent_p;
+        ((nodo_p != sent_p) && (nodo_p->valore != valore));
+        padre_p = nodo_p, nodo_p = (valore < nodo_p->valore)?
+                                     nodo_p->sx_p:
+                                     nodo_p->dx_p);
+    if(nodo_p != sent_p)
+        inserito = 0;
+    else
+    {
+        inserito = 1;
+        nuovo_p = (nodo_albero_bin_rn_t *)malloc(sizeof(nodo_albero_bin_rn_t));
+        nuovo_p->valore = valore;
+        nuovo_p->colore = rosso;
+        nuovo_p->sx_p = nuovo_p->dx_p = sent_p;
+        nuovo_p->padre_p = padre_p;
+        if(padre_p == sent_p)
+            sent_p->sx_p = sent_p->dx_p = nuovo_p;
+        else
+            if(valore < padre_p->valore)
+                padre_p->sx_p = nuovo_p;
+            else
+                padre_p->dx_p = nuovo_p;
+        ripristina_ins_albero_bin_ric_rn(sent_p, nuovo_p);
+    }
+    return(inserito);
+}
+
+void ripristina_ins_albero_bin_ric_rn(nodo_albero_bin_rn_t *sent_p, nodo_albero_bin_rn_t *nodo_p)
+{
+    nodo_albero_bin_rn_t *zio_p;
+    
+    while(nodo_p->padre_p->colore == rosso)
+        if(nodo_p->padre_p == nodo_p->padre_p->padre_p->sx_p)
+        {
+            zio_p = nodo_p->padre_p->padre_p->dx_p;
+            if(zio_p->colore == rosso)
+            {
+                nodo_p->padre_p->colore = nero;
+                zio_p->colore = nero;
+                nodo_p->padre_p->padre_p->colore = rosso;
+                nodo_p = nodo_p->padre_p->padre_p;
+            }
+            else
+            {
+                if(nodo_p == nodo_p->padre_p->dx_p)
+                {
+                    nodo_p = nodo_p->padre_p;
+                    ruota_sx(sent_p, nodo_p);
+                }
+                nodo_p->padre_p->colore = nero;
+                nodo_p->padre_p->padre_p->colore = rosso;
+                ruota_dx(sent_p, nodo_p->padre_p->padre_p);
+            }
+        }
+        else
+        {
+            zio_p = nodo_p->padre_p->padre_p->sx_p;
+            if(zio_p->colore == rosso)
+            {
+                nodo_p->padre_p->colore = nero;
+                zio_p->colore = nero;
+                nodo_p->padre_p->padre_p->colore = rosso;
+                nodo_p = nodo_p->padre_p->padre_p;
+            }
+            else
+            {
+                if(nodo_p == nodo_p->padre_p->sx_p)
+                {
+                    nodo_p = nodo_p->padre_p;
+                    ruota_dx(sent_p, nodo_p);
+                }
+                nodo_p->padre_p->colore = nero;
+                nodo_p->padre_p->padre_p->colore = rosso;
+                ruota_sx(sent_p, nodo_p->padre_p->padre_p);
+            }
+        }
+        sent_p->sx_p->colore = nero;
+}
