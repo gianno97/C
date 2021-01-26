30568a16a2b866e3d8ef2640409c86f85d3420a2
gianno97 <fabio.giannoccoli@gmail.com>
2021-01-26 22:58:41 +0100

    Update

--- C_workspace/Implementazione_albero_rn/main.c
+++ C_workspace/Implementazione_albero_rn/main.c
@@ -14,16 +14,67 @@ typedef struct nodo_albero_bin_rn
     struct nodo_albero_bin_rn *sx_p, *dx_p, *padre_p;
 } nodo_albero_bin_rn_t;
 
+typedef struct albero_rn
+{
+    nodo_albero_bin_rn_t *radice;
+    nodo_albero_bin_rn_t *sent_p;
+} albero_rn_t;
+
 int inserisci_in_albero_bin_ric_rn(nodo_albero_bin_rn_t *sent_p, int valore);
 nodo_albero_bin_rn_t *cerca_in_albero_bin_ric_rn(nodo_albero_bin_rn_t *sent_p, int valore);
 void ripristina_ins_albero_bin_ric_rn(nodo_albero_bin_rn_t *sent_p, nodo_albero_bin_rn_t *nodo_p);
 void ruota_sx(nodo_albero_bin_rn_t *sent_p, nodo_albero_bin_rn_t *x_p);
 void ruota_dx(nodo_albero_bin_rn_t *sent_p, nodo_albero_bin_rn_t *y_p);
+albero_rn_t* nuovo_albero_rn();
+nodo_albero_bin_rn_t* nuovo_nodo_albero(int valore);
 
 int main(void)
 {
+    albero_rn_t *t = nuovo_albero_rn();
+    
+    nodo_albero_bin_rn_t *a, *b, *c, *d, *e, *f, *g, *h, *i, *j, *k, *l, *m;
+    a = nuovo_nodo_albero(10);
+    b = nuovo_nodo_albero(20);
+    c = nuovo_nodo_albero(30);
+    d = nuovo_nodo_albero(100);
+    e = nuovo_nodo_albero(90);
+    f = nuovo_nodo_albero(40);
+    g = nuovo_nodo_albero(50);
+    h = nuovo_nodo_albero(60);
+    i = nuovo_nodo_albero(70);
+    j = nuovo_nodo_albero(80);
+    k = nuovo_nodo_albero(150);
+    l = nuovo_nodo_albero(110);
+    m = nuovo_nodo_albero(120);
     
     return (0);
+}
+
+nodo_albero_bin_rn_t* nuovo_nodo_albero(int valore)
+{
+    nodo_albero_bin_rn_t* n = malloc(sizeof(nodo_albero_bin_rn_t));
+    n->sx_p = NULL;
+    n->dx_p = NULL;
+    n->padre_p = NULL;
+    n->valore = valore;
+    n->colore = rosso;
+
+    return n;
+}
+
+albero_rn_t* nuovo_albero_rn()
+{
+    albero_rn_t *t = malloc(sizeof(albero_rn_t));
+    nodo_albero_bin_rn_t *sent_p_node = malloc(sizeof(nodo_albero_bin_rn_t));
+    sent_p_node->sx_p = NULL;
+    sent_p_node->dx_p = NULL;
+    sent_p_node->padre_p = NULL;
+    sent_p_node->colore = nero;
+    sent_p_node->valore = 0;
+    t->sent_p = sent_p_node;
+    t->radice = t->sent_p;
+
+    return t;
 }
 
 void ruota_sx(nodo_albero_bin_rn_t *sent_p, nodo_albero_bin_rn_t *x_p)
