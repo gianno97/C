2a8f258d0611a6499e1f627c6f4b8b096f6131fb
gianno97 <fabio.giannoccoli@gmail.com>
2021-06-02 00:15:32 +0200

    Update

--- C_workspace/Composizione_e_differenza_simmetrica/main.c
+++ C_workspace/Composizione_e_differenza_simmetrica/main.c
@@ -10,7 +10,13 @@ typedef struct elem_lista
     struct elem_lista *succ_p;
 } elem_lista_t;
 
+void acquisizione_relazione_binaria(int *insieme_finito_num_naturali,
+                                    int grandezza_insieme,
+                                    elem_lista_t *testa_p,
+                                    char stringa_relazioni_binarie[],
+                                    char stringa_relazione_binaria[]);
 void visita_lista(elem_lista_t *testa_p);
+int acquisizione_insieme(int *insieme_finito_num_naturali);
 int ricerca_lineare_array(int a[],
                           int n,
                           int valore);
@@ -18,8 +24,7 @@ int inserisci_in_lista_ordinata(elem_lista_t **testa_p,
                                 int valore_uno,
                                 int valore_due);
 void differenza_simmetrica(elem_lista_t *testa_p,
-                           elem_lista_t *testa_p_due,
-                           elem_lista_t **testa_p_diff);
+                           elem_lista_t *testa_p_due);
 void acquisizione_coppia(int *insieme_finito_num_naturali,
                          int grandezza_insieme,
                          int coppia_rel_bin[]);
@@ -30,23 +35,95 @@ void composizione_ricorsiva(elem_lista_t *elem_p,
 int main(int argc, char **argv)
 {
     int *insieme_finito_num_naturali;
-    int grandezza_insieme;
-    int i;
-    int j;
-    int contatore_numeri_insieme = 0;
     int esito_lettura;
     int coppia_prima_rel_bin[2] = {-1, -1};
     int coppia_sec_rel_bin[2] = {-1, -1};
     elem_lista_t *testa_p = NULL;
     elem_lista_t *testa_p_due = NULL;
-    elem_lista_t *testa_p_diff = NULL;
     int inserito;
     int fine_inser_prima_rel_bin = 1;
     int fine_inser_sec_rel_bin = 1;
     int valore_inserito = -1;
-    int numero_presente;
-    int numero_naturale;
+    char stringa_relazioni_binarie[] = {"Digita due relazioni binarie sull'insieme acquisito da tastiera.\n"};
+    char stringa_prima_relazione_binaria[] = {"Digita la prima relazione binaria.\n"};
+    char stringa_seconda_relazione_binaria[] = {"Digita la seconda relazione binaria.\n"};
     
+    /*Composizione*/
+    composizione_ricorsiva(testa_p, testa_p_due, testa_p_due);
+    /*Differenza simmetrica*/
+    differenza_simmetrica(testa_p, testa_p_due);
+    
+    return 0;
+}
+
+void acquisizione_relazione_binaria(int *insieme_finito_num_naturali,
+                                    int grandezza_insieme,
+                                    elem_lista_t *testa_p,
+                                    char stringa_relazioni_binarie[],
+                                    char stringa_relazione_binaria[])
+{
+    int fine_inser_rel_bin = 1;
+    int coppia_rel_bin[2] = {-1, -1};
+    int inserito,
+        esito_lettura;
+    int valore_inserito = -1;
+    
+    while(fine_inser_rel_bin == 1)
+    {
+        printf("Digita il primo numero della coppia:\n");
+        acquisizione_coppia(insieme_finito_num_naturali, grandezza_insieme, coppia_rel_bin);
+        printf("Digita il secondo numero della coppia:\n");
+        acquisizione_coppia(insieme_finito_num_naturali, grandezza_insieme, coppia_rel_bin);
+        inserito = inserisci_in_lista_ordinata(&testa_p, coppia_rel_bin[0], coppia_rel_bin[1]);
+        coppia_rel_bin[0] = -1;
+        coppia_rel_bin[1] = -1;
+        if(inserito == 1)
+            printf("Coppia inserita\n");
+        else
+            printf("Coppia gia' presente all'interno della relazione binaria\n");
+        
+        visita_lista(testa_p);
+        printf("\n");
+        
+        do
+        {
+            printf("Digita 0 per terminare l'inserimento delle coppie per la relazione binaria.\n");
+            printf("Digita 1 per continuare l'inserimento.\n");
+            esito_lettura = scanf("%d", &valore_inserito);
+            
+            if(esito_lettura != 1)
+                printf("Input non valido!\n");
+            else
+            {
+                if(valore_inserito == 0)
+                {
+                    printf("Inserimento terminato\n");
+                    fine_inser_rel_bin = 0;
+                }
+                else if(valore_inserito == 1)
+                {
+                    printf("Proseguimento dell'inserimento\n"); 
+                }
+                else
+                    printf("Input non valido!\n");
+            }
+            while (getchar() != '\n');
+        }
+        while(valore_inserito != 0 && valore_inserito != 1);
+        valore_inserito = -1;
+    }
+}
+
+int acquisizione_insieme(int *insieme_finito_num_naturali)
+{
+    int esito_lettura,
+        grandezza_insieme,
+        numero_naturale,
+        numero_presente,
+        i,
+        j;
+    int contatore_numeri_insieme = 0;
+        
     do
     {
         printf("Digita la grandezza dell'insieme(1 <= grandezza <= 20):\n");
@@ -59,140 +136,38 @@ int main(int argc, char **argv)
     }
     while(esito_lettura != 1 || grandezza_insieme < 1 || grandezza_insieme > 20);
     
-    for(j = 0; j < grandezza_insieme; j++)
-        insieme_finito_num_naturali[j] = -1;
-    
-    printf("Digita uno alla volta i numeri di un insieme finito di numeri naturali{0, 1, 2, 3, 4...}:\n");
-    do
+    if(grandezza_insieme != 0)
     {
-        esito_lettura = scanf("%d", &numero_naturale);
-        if(esito_lettura != 1 || numero_naturale < 0)
-            printf("Inserimento non valido\n");
-        else
-        {
-            numero_presente = ricerca_lineare_array(insieme_finito_num_naturali, grandezza_insieme, numero_naturale);
-            if(numero_presente == -1)
-            {
-                insieme_finito_num_naturali[contatore_numeri_insieme] = numero_naturale;
-                contatore_numeri_insieme++;
-            }
-            else
-                printf("Il numero inserito e' gia' presente all'interno dell'insieme\n");
-        }
-        while (getchar() != '\n');
-    }
-    while(esito_lettura != 1 || numero_naturale < 0 || (contatore_numeri_insieme < grandezza_insieme));
+        for(j = 0; j < grandezza_insieme; j++)
+            insieme_finito_num_naturali[j] = -1;
     
-    printf("Insieme acquisito da tastiera:\n");
-    for(i = 0; i < grandezza_insieme; i++)
-        printf("%d ", insieme_finito_num_naturali[i]);
-    printf("\n");
-    
-    printf("Digita due relazioni binarie sull'insieme acquisito da tastiera.\n");
-    printf("Digita la prima relazione binaria.\n");
-    while(fine_inser_prima_rel_bin == 1)
-    {
-        printf("Digita il primo numero della coppia:\n");
-        acquisizione_coppia(insieme_finito_num_naturali, grandezza_insieme, coppia_prima_rel_bin);
-        printf("Digita il secondo numero della coppia:\n");
-        acquisizione_coppia(insieme_finito_num_naturali, grandezza_insieme, coppia_prima_rel_bin);
-        inserito = inserisci_in_lista_ordinata(&testa_p, coppia_prima_rel_bin[0], coppia_prima_rel_bin[1]);
-        coppia_prima_rel_bin[0] = -1;
-        coppia_prima_rel_bin[1] = -1;
-        if(inserito == 1)
-            printf("Coppia inserita\n");
-        else
-            printf("Coppia gia' presente all'interno della relazione binaria\n");
-        
-        visita_lista(testa_p);
-        printf("\n");
-        
+        printf("Digita uno alla volta i numeri di un insieme finito di numeri naturali{0, 1, 2, 3, 4...}:\n");
         do
         {
-            printf("Digita 0 per terminare l'inserimento delle coppie per la prima relazione binaria.\n");
-            printf("Digita 1 per continuare l'inserimento.\n");
-            esito_lettura = scanf("%d", &valore_inserito);
-            
-            if(esito_lettura != 1)
-                printf("Input non valido!\n");
+            esito_lettura = scanf("%d", &numero_naturale);
+            if(esito_lettura != 1 || numero_naturale < 0)
+                printf("Inserimento non valido\n");
             else
             {
-                if(valore_inserito == 0)
+                numero_presente = ricerca_lineare_array(insieme_finito_num_naturali, grandezza_insieme, numero_naturale);
+                if(numero_presente == -1)
                 {
-                    printf("Inserimento terminato\n");
-                    fine_inser_prima_rel_bin = 0;
-                }
-                else if(valore_inserito == 1)
-                {
-                    printf("Proseguimento dell'inserimento\n"); 
+                    insieme_finito_num_naturali[contatore_numeri_insieme] = numero_naturale;
+                    contatore_numeri_insieme++;
                 }
                 else
-                    printf("Input non valido!\n");
+                    printf("Il numero inserito e' gia' presente all'interno dell'insieme\n");
             }
             while (getchar() != '\n');
         }
-        while(valore_inserito != 0 && valore_inserito != 1);
-        valore_inserito = -1;
-    }
+        while(esito_lettura != 1 || numero_naturale < 0 || (contatore_numeri_insieme < grandezza_insieme));
     
-    printf("Digita la seconda relazione binaria.\n");
-    while(fine_inser_sec_rel_bin == 1)
-    {
-        printf("Digita il primo numero della coppia:\n");
-        acquisizione_coppia(insieme_finito_num_naturali, grandezza_insieme, coppia_sec_rel_bin);
-        printf("Digita il secondo numero della coppia:\n");
-        acquisizione_coppia(insieme_finito_num_naturali, grandezza_insieme, coppia_sec_rel_bin);
-        inserito = inserisci_in_lista_ordinata(&testa_p_due, coppia_sec_rel_bin[0], coppia_sec_rel_bin[1]);
-        coppia_sec_rel_bin[0] = -1;
-        coppia_sec_rel_bin[1] = -1;
-        if(inserito == 1)
-            printf("Coppia inserita\n");
-        else
-            printf("Coppia gia' presente all'interno della relazione binaria\n");
-        
-        visita_lista(testa_p_due);
+        printf("Insieme acquisito da tastiera:\n");
+        for(i = 0; i < grandezza_insieme; i++)
+            printf("%d ", insieme_finito_num_naturali[i]);
         printf("\n");
-        
-        do
-        {
-            printf("Digita 0 per terminare l'inserimento delle coppie per la seconda relazione binaria.\n");
-            printf("Digita 1 per continuare l'inserimento.\n");
-            esito_lettura = scanf("%d", &valore_inserito);
-            
-            if(esito_lettura != 1)
-                printf("Input non valido!\n");
-            else
-            {
-                if(valore_inserito == 0)
-                {
-                    printf("Inserimento terminato\n");
-                    fine_inser_sec_rel_bin = 0;
-                }
-                else if(valore_inserito == 1)
-                {
-                    printf("Proseguimento dell'inserimento\n"); 
-                }
-                else
-                    printf("Input non valido!\n");
-            }
-            while (getchar() != '\n');
-        }
-        while(valore_inserito != 0 && valore_inserito != 1);
-        valore_inserito = -1;
     }
-    
-    /*Composizione*/
-    printf("Composizione:\n");
-    composizione_ricorsiva(testa_p, testa_p_due, testa_p_due);
-    printf("\n");
-    
-    /*Differenza simmetrica*/
-    printf("Differenza simmetrica:\n");
-    differenza_simmetrica(testa_p, testa_p_due, &testa_p_diff);
-    visita_lista(testa_p_diff);
-    printf("\n");
-    
-    return 0;
+    return grandezza_insieme;
 }
 
 void acquisizione_coppia(int *insieme_finito_num_naturali, int grandezza_insieme, int coppia_rel_bin[])
@@ -236,12 +211,14 @@ void acquisizione_coppia(int *insieme_finito_num_naturali, int grandezza_insieme
     while(trovato == -1);
 }
 
-void differenza_simmetrica(elem_lista_t *testa_p, elem_lista_t *testa_p_due, elem_lista_t **testa_p_diff)
+void differenza_simmetrica(elem_lista_t *testa_p, elem_lista_t *testa_p_due)
 {
     elem_lista_t *elem_p_diff,
                  *elem_p_diff_due;
     int sentinella = 1;
+    elem_lista_t *testa_p_diff = NULL;
     
+    printf("Differenza simmetrica:\n");
     for(elem_p_diff = testa_p; (elem_p_diff != NULL); elem_p_diff = elem_p_diff->succ_p)
     {
         for(elem_p_diff_due = testa_p_due; (elem_p_diff_due != NULL) && (sentinella != 0); elem_p_diff_due = elem_p_diff_due->succ_p)
@@ -250,7 +227,7 @@ void differenza_simmetrica(elem_lista_t *testa_p, elem_lista_t *testa_p_due, ele
                 sentinella = 0;
             else
                 if(elem_p_diff_due->succ_p == NULL)
-                    inserisci_in_lista_ordinata(testa_p_diff, elem_p_diff->valore_uno, elem_p_diff->valore_due);
+                    inserisci_in_lista_ordinata(&testa_p_diff, elem_p_diff->valore_uno, elem_p_diff->valore_due);
         }
         sentinella = 1;
     }
@@ -263,10 +240,12 @@ void differenza_simmetrica(elem_lista_t *testa_p, elem_lista_t *testa_p_due, ele
                 sentinella = 0;
             else
                 if(elem_p_diff->succ_p == NULL)
-                    inserisci_in_lista_ordinata(testa_p_diff, elem_p_diff_due->valore_uno, elem_p_diff_due->valore_due);
+                    inserisci_in_lista_ordinata(&testa_p_diff, elem_p_diff_due->valore_uno, elem_p_diff_due->valore_due);
         }
         sentinella = 1;
     }
+    visita_lista(testa_p_diff);
+    printf("\n");
 }
 
 void composizione_ricorsiva(elem_lista_t *elem_p,
@@ -275,6 +254,7 @@ void composizione_ricorsiva(elem_lista_t *elem_p,
 {
     elem_lista_t *testa_p_comp = NULL;
     
+    printf("Composizione:\n");
     if(elem_p != NULL)
     {
         if(elem_p->valore_due == elem_p_due->valore_uno)
@@ -300,6 +280,7 @@ void composizione_ricorsiva(elem_lista_t *elem_p,
         }
     }
     visita_lista(testa_p_comp);
+    printf("\n");
 }
 
 int ricerca_lineare_array(int a[], int n, int valore)
